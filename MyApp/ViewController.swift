270b6075183b7f1d81b7448db628d0fb10f5cd59
Syed Abdul basit <>
2020-08-24 11:57:43 +0500

    - push

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -1,20 +1,54 @@
 //
 //  ViewController.swift
-//  MyApp
+//  RetirementCalculator
 //
-//  Created by Syed Abdul basit on 27/07/2020.
-//  Copyright © 2020 Syed Abdul basit. All rights reserved.
+//  Created by Eduardo Rosas on 8/19/19.
+//  Copyright © 2019 Eduardo Rosas. All rights reserved.
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfFirstNumber: UITextField!
+    @IBOutlet weak var tfSecondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if MSCrashes.hasCrashedInLastSession() {
+            let alert = UIAlertController(title: "Oops", message: "Sorry about that, an error occured.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's cool", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        MSAnalytics.trackEvent("navigated_to_calculator")
+    }
+    
+    func multiplyNumbers(_ firstNum: Int, secondNum: Int) -> Int {
+        return firstNum * secondNum
+    }
 
-
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+        // MSCrashes.generateTestCrash()
+        let firstNumber = Int(tfFirstNumber.text ?? "0")
+        let secondNumber = Int(tfSecondNumber.text ?? "0")
+        
+        if let firstNum = firstNumber, let secondNum = secondNumber {
+        let total = multiplyNumbers(firstNum, secondNum: secondNum)
+            
+              resultLabel.text = "The result is \(total)"
+        }
+        
+      
+   
+    }
+    
 }
 
